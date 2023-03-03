64a698f0a3a7ef6c84e2a8fff8d37158374b010b
joao-montanari <joao.unipbr@gmail.com>
2023-03-03 16:40:53 -0300

    continuacao do desenvolvimento do componente tabela

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -8,79 +8,113 @@
     ]
 
     return(
-        <div className="w-auto h-auto m-auto border border-gray-900">
-            <div className="border-b border-black w-auto h-10 flex">
+        <div className="max-w-7xl h-auto m-auto border border-gray-900">
+            <div className="border-b border-black w-auto h-12 flex">
                 <ul className="flex">
-                    <li className="border-r border-black h-full w-48 text-center py-1.5">
+                    <li className="border-r border-black h-full w-40 text-center py-3 font-bold bg-gray-200">
                         Horários
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         ADM1 (M)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         ADM2 (T)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         DS3 (T)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         DS4 (T)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         DS5 (M)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         DS6 (M)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         DS7 (T)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         MD3 (T)
                     </li>
-                    <li className="border-r border-black h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         MEC 1º Ano
                     </li>
-                    <li className="h-full w-28 text-center py-1.5">
+                    <li className="border-r border-black h-full w-28 text-center py-3 font-bold bg-gray-200">
                         MEC 2º Ano
                     </li>
                 </ul>
             </div>
-            <ul className="border-r border-black h-full w-48 grid">
-                <li className="border-b border-black text-center py-2">
-                    7:30
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    08:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    09:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    10:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    11:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    12:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    13:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    14:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    15:00
-                </li>
-                <li className="border-b border-black text-center py-2">
-                    16:00
-                </li>
-                <li className="text-center py-2">
-                    17:00
-                </li>
-            </ul>
+            <div className="flex">
+                <ul className="border-r border-black h-full w-40 grid">
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        7:30
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        08:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        09:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        10:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        11:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        12:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        13:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        14:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        15:00
+                    </li>
+                    <li className="border-b border-black text-center py-4 font-bold text-xl bg-gray-200">
+                        16:00
+                    </li>
+                    <li className="text-center py-4 font-bold text-xl bg-gray-200">
+                        17:00
+                    </li>
+                </ul>
+                <div className="flex">
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div className="w-28 border-r border-black">
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
  }
